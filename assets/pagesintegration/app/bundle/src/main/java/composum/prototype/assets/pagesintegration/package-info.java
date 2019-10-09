ed67944199a754bb3d0dda015f2566fc0bb9c7e4
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2019-10-09 15:20:33 +0200

    naked archetype for pagesintegration

---
+++ assets/pagesintegration/app/bundle/src/main/java/composum/prototype/assets/pagesintegration/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Example for Pages integration of Assets
+ * Test- and Exampleproject of the integration of the Assets framework into Composum Pages
+ */
+package composum.prototype.assets.pagesintegration;
---
+++ assets/pagesintegration/app/bundle/src/test/java/composum/prototype/assets/pagesintegration/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Tests for Example for Pages integration of Assets.
+ */
+package composum.prototype.assets.pagesintegration;
